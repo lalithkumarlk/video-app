40ae3ca321fadf33bb541ec54275b29a1a4a8cfd
Lalith Kumar <lalith.kumar@humanfractal.io>
2021-10-10 00:05:51 +0530

    Enabled Sound

--- src/components/TimeInterval.tsx
+++ src/components/TimeInterval.tsx
@@ -1,38 +1,38 @@
-import { useContext, useEffect, useRef, useState } from "react"
+import { useContext, useEffect, useReducer, useRef, useState } from "react"
 
 export interface Props {
     timer : number
 }
 
-
 const TimeInterval: React.FC<Props> = (data) => {
 
-    const [seconds, setSeconds] = useState(-1);
+    const [counter, setCounter] = useState(data.timer);
 
-    const [ isPlaySound,setPlaySound] = useState(false)
-
+    const playSound = () => {
+      const AUDIO_URL = `https://assets.coderrocketfuel.com/pomodoro-times-up.mp3`;
+      new Audio(AUDIO_URL).play();
+    }
 
     useEffect(() => {
-
-        setSeconds(data.timer)
-
+        setCounter(data.timer)
         const interval  = setInterval(() => {
-            setSeconds(seconds => seconds > 0 ? seconds - 1 : 0);
-            
-          }, 1000)
-
-        return () => clearInterval(interval);
-
+            setCounter(counter =>counter > 0 ? counter-1 : 0);
+        }, 1000)
+  
+       return () => clearInterval(interval);
+       
       },[data]);
 
+
+    useEffect(()=>{
+        counter === 0 && playSound()
+      },[counter])
+
+       
     return (
         <div className="App">
         <header className="App-header">
-
-            <h1>Timer : {data.timer}</h1>
-            <h1>Second : {seconds}</h1>
-            
-
+            <h1>Second : { counter}</h1>
         </header>
       </div>
     )
